971fa730e58b0f5d7672c4b782eecdf300e63a4e
garciagenrique <enrique.garcia.garcia@cern.ch>
2024-02-08 10:47:10 +0100

    showing image in the panel !

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,15 @@ import {
 import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 import { Widget } from '@lumino/widgets';
 
+interface APODResponse {
+  copyright: string;
+  dateL: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+}
+
 /**
  * Initialization data for the engarcia_js_package_name extension.
  */
@@ -14,30 +23,56 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'Test and Dev of JupyterLab extensions.',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension engarcia_js_package_name is activated!!');
 
     // Define a widget creator function,
     // then call it to make a new widget
-    const newWidget = () => {
+    const newWidget = async () => {
       // Create a blank content widget inside of a MainAreaWidget
       const content = new Widget();
       const widget = new MainAreaWidget({ content });
       widget.id = 'jlab-engarcia';
       widget.title.label = 'Astronomy Picture';
       widget.title.closable = true;
+
+      // Part 3 of the tutoriakl
+      // Add an image to the content
+      let img = document.createElement('img');
+      content.node.appendChild(img);
+
+      // Get a random daty
+      function randomDate() {
+        const start = new Date(2023, 6, 3);
+        const end = new Date();
+        const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
+        return randomDate.toISOString().slice(0,10);
+      }
+
+      // Show infor about a random pic
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
+      const data = await response.json() as APODResponse;
+
+      if (data.media_type === 'image') {
+        // Populate the image
+        img.src = data.url;
+        img.title = data.title;
+      } else {
+        console.log('Random APOD was not a picture');
+      }
+
       return widget;
     }
-    let widget = newWidget();
+    let widget = await newWidget();
 
     // Add an app command
     const command: string = 'apod:open';
     app.commands.addCommand(command, {
       label: 'Random Astronomy Picture',
-      execute: () => {
-        //Regenerate the wodget if disposed
+      execute: async () => {
+        //Regenerate the widget if disposed
         if (widget.isDisposed) {
-          widget = newWidget();
+          widget = await newWidget();
         }
         if (!widget.isAttached) {
           // Attach the widget to the main work 
